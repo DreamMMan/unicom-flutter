4e7749c6c7b83178a547394fe69a8f0085701790
liangyt <liangyt@igelian.com>
2020-01-13 11:14:45 +0800

    退出提示

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -2,7 +2,7 @@
  * @Author: liangyt
  * @Date: 2019-12-18 10:50:53
  * @LastEditors  : liangyt
- * @LastEditTime : 2020-01-03 15:35:28
+ * @LastEditTime : 2020-01-13 11:10:54
  * @Description: 首页
  * @FilePath: /unicom_flutter/lib/pages/homePage.dart
  */
@@ -12,6 +12,7 @@ import 'package:unicom_flutter/constant/myConstant.dart';
 import 'package:unicom_flutter/pages/alarmPage.dart';
 import 'package:unicom_flutter/pages/orderPage.dart';
 import 'package:unicom_flutter/providers/homeProvide.dart';
+import 'package:unicom_flutter/utils/index.dart';
 import 'package:unicom_flutter/widgets/common/myAsset.dart';
 
 class HomePage extends StatefulWidget {
@@ -44,25 +45,40 @@ class _HomePageState extends State<HomePage> {
         title: Text('通知'))
   ];
 
+  DateTime lastPopTime;
+
   @override
   Widget build(BuildContext context) {
-    return Provide<HomeProvide>(
-      builder: (context, child, data) {
-        return Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-            items: tabs,
-            currentIndex: data.current,
-            unselectedFontSize: 14,
-            onTap: (int index) {
-              Provide.value<HomeProvide>(context).setCurrent(index);
-            },
-          ),
-          body: IndexedStack(
-            index: data.current,
-            children: pageList,
-          ),
-        );
+    return WillPopScope(
+      onWillPop: () async {
+        if (lastPopTime == null ||
+            DateTime.now().difference(lastPopTime) > Duration(seconds: 2)) {
+          Utils.showToast('再试一次退出');
+          setState(() {
+            lastPopTime = DateTime.now();
+          });
+          return Future.value(false);
+        }
+        return Future.value(true);
       },
+      child: Provide<HomeProvide>(
+        builder: (context, child, data) {
+          return Scaffold(
+            bottomNavigationBar: BottomNavigationBar(
+              items: tabs,
+              currentIndex: data.current,
+              unselectedFontSize: 14,
+              onTap: (int index) {
+                Provide.value<HomeProvide>(context).setCurrent(index);
+              },
+            ),
+            body: IndexedStack(
+              index: data.current,
+              children: pageList,
+            ),
+          );
+        },
+      ),
     );
   }
 }
